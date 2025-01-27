143249e712080c0dff7d0144d3dd1188e06aa64b
Volvapp <chriszp2905@gmail.com>
2025-01-27 14:56:58 +0200

    init commit

---
+++ src/test/java/org/logisticcompany/LogisticCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package org.logisticcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticCompanyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
